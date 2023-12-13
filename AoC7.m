056c0bdaa3d365e9d0dc4870d6c6a871d85f6bd2
s2053012 <a.a.keizer@student.utwente.nl>
2023-12-13 19:57:54 +0100

    dag 7 deel 2, heel veel for loops splitten in deel 2, revenge of the for loop i guess.

--- AoC7.m
+++ AoC7.m
@@ -178,21 +178,33 @@ toc;
 
 %% part 2
 
+%HALLO BEZOEKERS! DIT IS DEEL 2!!! 
+%Het was een vergissing om Alexander Keizer een MATLAB licensie te geven...
+
+
+
+
+
+
+
 %TO DO: redo this with joker rule: J is now lowest value, but can pretend
 %to be any value to increase scorerank. 
-
+tic;
 typelist = [A;K;Q;T;n9;n8;n7;n6;n5;n4;n3;n2;J]; %new typelist lmfao
 
 scorerank = zeros(length(data.textdata),1);
 valuematrix = zeros(length(data.textdata),5); %YOU GOTTA VALUE VALUE
 
-for i = 1:length(data.textdata)     %Ik denk in for loopjes...
+fullh = false;
+doublepair = false;
+
+for i = 1:length(data.textdata)     %We checken niet voor J.
     
     %Ook al check je met het sorteren juist op het eind welke waarde elke kaart
     %heeft, doe ik het in het begin om de lezer te verwarren. 
     for p = 1:5
         VAL = extract(data.textdata{i},p); %verassings caps lock, geniet ervan!
-        for q = 1:length(typelist)
+        for q = 1:length(typelist)  %hier checken we wel voor J voor value. 
             if contains(VAL,typelist(q))
                valuematrix(i,p) =  14 - q;
             end
@@ -203,18 +215,36 @@ for i = 1:length(data.textdata)     %Ik denk in for loopjes...
             %after your for loop that had a for loop inside of it, whilst I
             %am in a for loop. 
             
-    for k = 1:length(typelist)
+    for k = 1:(length(typelist)-1)
         
+        %typelist(13) is J, "there is another" as they say. 
+        %WOW dit rijmt!
         
-        if count(data.textdata{i},typelist(k)) == 5
+        if count(data.textdata{i},[typelist(k) typelist(13)]) == 5
         
             scorerank(i) = 7;
             break;
         end
+        
+    end
     
-        if count(data.textdata{i},typelist(k)) == 4
+    for k = 1:(length(typelist)-1)
+        if scorerank(i) == 7
+           break; 
+        end
+        
+        if count(data.textdata{i},[typelist(k) typelist(13)]) == 4
         
             scorerank(i) = 6;
+            break;
+        end
+    end
+    
+    for k = 1:(length(typelist)-1)
+        if scorerank(i) == 7
+            break;
+        end
+        if scorerank(i) == 6
             break;
         end
         
@@ -224,10 +254,10 @@ for i = 1:length(data.textdata)     %Ik denk in for loopjes...
                     %dit betekent een for loop met length k-1, sinds we
                     %niet hogere waardes meer kunnen vinden. Correctie, dit is niet waar   
                     %We gaan nu gewoon beide kanten op kijken. 
-        if (count(data.textdata{i},typelist(k)) == 3) || (count(data.textdata{i},typelist(k)) == 2)
-            if (count(data.textdata{i},typelist(k)) == 3)
-                if ~(k == 13) %geen full house als je 3x 2 op het eind vindt.
-                    for b = 1:(13-k) %wordt jij niet opgewonden van een conditionele for loop?
+        if (count(data.textdata{i},[typelist(k) typelist(13)]) == 3) || (count(data.textdata{i},[typelist(k) typelist(13)]) == 2)
+            if (count(data.textdata{i},[typelist(k) typelist(13)]) == 3)
+                if ~(k == 12) %geen full house als je 3x 2 op het eind vindt.
+                    for b = 1:(12-k) %wordt jij niet opgewonden van een conditionele for loop?
                         if count(data.textdata{i},typelist(k+b)) == 2   %check lower values
                             scorerank(i) = 5;
                             fullh = true;
@@ -242,8 +272,8 @@ for i = 1:length(data.textdata)     %Ik denk in for loopjes...
                         end
                     end
                 end
-                if k == 13
-                    for d = 1:12
+                if k == 12
+                    for d = 1:11
                         if count(data.textdata{i},typelist(d)) == 2 %check higher values again
                             scorerank(i) = 5;
                             fullh = true;
@@ -260,8 +290,8 @@ for i = 1:length(data.textdata)     %Ik denk in for loopjes...
                 end
             end
             
-            if (count(data.textdata{i},typelist(k)) == 2)
-                for f = 1:(13-k)
+            if (count(data.textdata{i},[typelist(k) typelist(13)]) == 2)
+                for f = 1:(12-k)
                     if count(data.textdata{i},typelist(k+f)) == 3   %check lower values
                             scorerank(i) = 5;
                             %fullh = true;
@@ -270,25 +300,38 @@ for i = 1:length(data.textdata)     %Ik denk in for loopjes...
             end
             fullh = false;  %reset fullh boolean. 
             
-            if (count(data.textdata{i},typelist(k)) == 2) && (~(scorerank(i)==5))
+            if (count(data.textdata{i},[typelist(k) typelist(13)]) == 2) && (~(scorerank(i)==5))
             %we do not add break here so that we can still see if we have
             %one or two two pair. 
             else
                 break;
             end
         end
-        
-        
+    end
+    
+    for k = 1:(length(typelist)-1)
+        if scorerank(i) == 7
+            break;
+        end
+        if scorerank(i) == 6
+            break;
+        end
+        if scorerank(i) == 5 
+            break;
+        end
+        if scorerank(i) == 4
+            break;
+        end
         %nu nog even 2 pair en one pair berekenen. We gebruiken dezelfde
         %logica als bij full house, maar nu 2x checken of we een paar
         %hebben. Natuurlijk kijken we alleen voor waardes die we nog moeten
         %checken, dus lagere waardes. Er kan geen situatie ontstaan dat wij
         %bijv. 2x een 3 vinden, en we de 2x 9 nog niet hebben gevonden. 
         
-        if count(data.textdata{i},typelist(k)) == 2  
-            if ~(k == 13)
-                for e = 1:(13-k)
-                    if count(data.textdata{i},typelist(k+e)) == 2
+        if (count(data.textdata{i},[typelist(k) typelist(13)]) == 2) && (count(data.textdata{i},typelist(13)) == 1) || (count(data.textdata{i},typelist(k))==2)
+            if (count(data.textdata{i},typelist(k))==2)
+                for e = 1:(12-k)    %checking lower values
+                    if count(data.textdata{i},[typelist(k+e) typelist(13)]) == 2
                         scorerank(i) = 3;
                         doublepair = true;
                         %break;
@@ -309,8 +352,26 @@ for i = 1:length(data.textdata)     %Ik denk in for loopjes...
         %break dus zou goed moeten komen. 
         
             scorerank(i) = 1;
+    
     end
     %clear fullh
 end
+
+
+check1 = sum(scorerank==1,'all');
+check2 = sum(scorerank==2,'all');
+check3 = sum(scorerank==3,'all');
+check4 = sum(scorerank==4,'all');
+check5 = sum(scorerank==5,'all');
+check6 = sum(scorerank==6,'all');
+check7 = sum(scorerank==7,'all');
+supermatrix = cat(2,data.data,scorerank);
+supermatrix = cat(2,supermatrix,valuematrix);
+
+supermatrix = sortrows(supermatrix, [2 3 4 5 6 7]);
+rank = 1:1000;
+
+answer = sum(supermatrix(:,1).*rank');
+toc;
 
 
